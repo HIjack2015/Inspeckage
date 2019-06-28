928bfd0c8ca9d75ff47138c144ae274b6865cd9d
jiakang <1079153785@qq.com>
2019-06-29 00:23:03 +0800

    adapt android 8.0 foreground service

--- app/src/main/java/mobi/acpm/inspeckage/webserver/InspeckageService.java
+++ app/src/main/java/mobi/acpm/inspeckage/webserver/InspeckageService.java
@@ -1,9 +1,15 @@
 package mobi.acpm.inspeckage.webserver;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.IBinder;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.NotificationCompat;
 import android.widget.Toast;
 
 import java.io.IOException;
@@ -46,6 +52,25 @@ public class InspeckageService extends Service {
         }
 
         Toast.makeText(this, "Service started on port " + port, Toast.LENGTH_LONG).show();
+
+        //android 8.0 need a ongoing notification for foreground service
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            String CHANNEL_ID = "my_channel_01";
+            NotificationChannel channel = null;
+            channel = new NotificationChannel(CHANNEL_ID,
+                    "Channel human readable title",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            ((NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE)).createNotificationChannel(channel);
+
+            Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+                    .setContentTitle("")
+                    .setContentText("").build();
+
+            startForeground(1, notification);
+        }
+
+
+
         return START_STICKY;
     }
 
